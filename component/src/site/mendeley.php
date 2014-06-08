65011ccfa9665f416320c230e75ea65cb9a3877a
Alexey Vladykin <vladykin@users.noreply.github.com>
2014-06-09 01:23:08 +0400

    download file only if it does not exist

--- component/src/site/mendeley.php
+++ component/src/site/mendeley.php
@@ -11,13 +11,14 @@ $docId = $input->getUint('doc');
 $fileHash = $input->getAlnum('file');
 
 if ($user && $docId && $fileHash) {
-    $accessToken = MendeleyTokenDB::getAccessToken($user);
-    $mendeleySession = new \mendeley\Session($accessToken);
-
     $params = JComponentHelper::getParams('com_mendeley');
     $destFileRel = '/' . $params->get('storage_folder') . '/' . $docId . '.' . $fileHash . '.pdf';
     $destFileAbs = JPATH_BASE . $destFileRel;
-    $mendeleySession->downloadFile($docId, $fileHash, $destFileAbs);
+    if (!file_exists($destFileAbs)) {
+        $accessToken = MendeleyTokenDB::getAccessToken($user);
+        $mendeleySession = new \mendeley\Session($accessToken);
+        $mendeleySession->downloadFile($docId, $fileHash, $destFileAbs);
+    }
     header('Location: ' . JURI::base(true) . $destFileRel);
     JFactory::getApplication()->close();
 }
